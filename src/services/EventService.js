1b267f13b4c14b3a81e5dd40b9bc2011b8be7777
Gaurav Gupta <gauravgupta1425@gmail.com>
2023-06-17 22:49:27 +0530

    first commit

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const EVENT_API_BASE_URL = "https://eventmanagementbackend-production.up.railway.app/api/v1/events";
-const EVENT_API_BASE_URL = "http://localhost:8080/api/v1/events";
+const EVENT_API_BASE_URL = "https://eventmanagementbackend-production.up.railway.app/api/v1/events";
+// const EVENT_API_BASE_URL = "http://localhost:8080/api/v1/events";
 
 class EventService {
   getEvents() {
